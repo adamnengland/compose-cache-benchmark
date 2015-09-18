b25ec2f3550e52e2cdab252a0128c89e6bfff689
Adam N England <adam.n.england@gmail.com>
2015-09-18 09:48:49 -0500

    Temp

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,17 @@ module ComposeCacheBenchmark
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Memcache Localhost
+    #config.cache_store = :dalli_store,["localhost:11211"],{:failover => true,:socket_timeout => 1.5,:socket_failure_delay => 0.2}
+
+    # memcachier
+    config.cache_store = :dalli_store,["mc5.dev.ec2.memcachier.com:11211"],{:username => "426773",:password => "e0b11edf107b4e5c23a248995cc11606",:failover => true,:socket_timeout => 1.5,:socket_failure_delay => 0.2}
+
+    #redis local
+    #config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
+
+    #redis2go
+    #config.cache_store = :redis_store, 'redis://herring.redistogo.com:9307/', {username: 'redistogo', password: '82e4cd9c1acb9e4802cfd1a66e444c6d'}
   end
 end
--- lib/tasks/cache_benchmark.rake
+++ lib/tasks/cache_benchmark.rake
@@ -1,13 +1,17 @@
 namespace :cache_benchmark do
   task test: :environment do
     cacheval = [(0..1000).map{ rand(36).to_s(36) }.join]
-    Benchmark.bm(15) { |x|
+    Benchmark.bm { |x|
       x.report("caching:") {
-        10000.times do
-          Rails.cache.write("test", cacheval)
-          Rails.cache.fetch("test")
-          Rails.cache.delete("test")
+        Rails.cache.write("test", cacheval)
+        1000.times do
+          if !Rails.cache.exist?("test")
+            raise
+          else
+            Rails.cache.fetch("test")
+          end
         end
+        Rails.cache.delete("test")
       }
     }
   end
